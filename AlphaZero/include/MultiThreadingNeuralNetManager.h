adecda8c217908cc90f6d19aaae83c0018186488
DESKTOP-3K85TKB\Georg <g.steinhauser1@web.de>
2021-04-30 23:13:47 +0200

    Implemented the stub of the MultiThreadingNeuralNetManager

--- AlphaZero/include/MultiThreadingNeuralNetManager.h
+++ AlphaZero/include/MultiThreadingNeuralNetManager.h
@@ -1,13 +1,23 @@
 #ifndef DEEPREINFORCEMENTLEARNING_MULTITHREADINGNEURALNETMANAGER_H
 #define DEEPREINFORCEMENTLEARNING_MULTITHREADINGNEURALNETMANAGER_H
 
+#include <torch/torch.h>
+#include <mutex>
+
 class MultiThreadingNeuralNetManager 
 {
 public:
-
+	MultiThreadingNeuralNetManager();
+	void addInputThreadSafe(torch::Tensor input);
+	void calculateOutputThreadSafe();
+	void clearInput();
+	torch::Tensor getOutput();
 
 private:
-
+	torch::Tensor inputBuffer;
+	torch::Tensor output;
+	std::mutex threadingMutex;
+	int elementsAdded = 0;
 };
 
 #endif //DEEPREINFORCEMENTLEARNING_MULTITHREADINGNEURALNETMANAGER_H
--- AlphaZero/src/MultiThreadingNeuralNetManager.cpp
+++ AlphaZero/src/MultiThreadingNeuralNetManager.cpp
@@ -1,3 +1,34 @@
 #include "MultiThreadingNeuralNetManager.h"
 
+MultiThreadingNeuralNetManager::MultiThreadingNeuralNetManager()
+{
+	elementsAdded = 0;
+}
+
+void MultiThreadingNeuralNetManager::addInputThreadSafe(torch::Tensor input)
+{
+	std::unique_lock<std::mutex> lock(this->threadingMutex);
+
+	if (this->inputBuffer.numel() == 0)
+		this->inputBuffer = input;
+	else
+		this->inputBuffer = torch::cat({ this->inputBuffer, input}, 0);
+	this->elementsAdded++;
+}
+
+void MultiThreadingNeuralNetManager::calculateOutputThreadSafe()
+{
+}
+
+void MultiThreadingNeuralNetManager::clearInput()
+{
+	this->inputBuffer = torch::Tensor();
+	this->elementsAdded = 0;
+}
+
+
+torch::Tensor MultiThreadingNeuralNetManager::getOutput()
+{
+	return torch::Tensor();
+}
 
