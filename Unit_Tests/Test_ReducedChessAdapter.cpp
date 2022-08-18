ea4b1aa2aa16584a46c62783de9fca5ed5850d80
DESKTOP-3K85TKB\Georg <g.steinhauser1@web.de>
2022-08-18 23:19:30 +0200

    Add some unit tests for the chess adapter

--- Unit_Tests/Test_ReducedChessAdapter.cpp
+++ Unit_Tests/Test_ReducedChessAdapter.cpp
@@ -50,3 +50,45 @@ TEST(ReducedChessAdapter, test_get_next_Player_for_Player_two)
 
 	ASSERT_EQ(adap.getNextPlayer(player), static_cast<int>(ceg::PieceColor::WHITE));
 }
+
+TEST(ReducedChessAdapter, test_make_move_1) 
+{
+	const std::string boardStr = "rnbqkbnr/pp1ppppp/8/2p5/3P4/8/PPP1PPPP/RNBQKBNR w KQkq -";
+	ceg::BitBoard board(boardStr);
+	int iMove = chess::getIntFromMove(ceg::Move(3, 4, 2, 3));
+
+	auto res = adap.makeMove(ceg::to_FEN_string(board, false), iMove, static_cast<int>(ceg::PieceColor::WHITE));
+
+	ASSERT_EQ(res, "rnbqkbnr/pp1ppppp/8/2P5/8/8/PPP1PPPP/RNBQKBNR b KQkq -");
+}
+
+TEST(ReducedChessAdapter, test_get_player_won_black)
+{
+	const std::string boardStr = "3k4/8/8/8/8/8/6r1/3K3r w - -";
+	int playerWon = adap.getPlayerWon(boardStr);
+
+	ASSERT_EQ(playerWon, static_cast<int>(ceg::PieceColor::BLACK));
+}
+
+TEST(ReducedChessAdapter, test_get_player_won_white)
+{
+	const std::string boardStr = "3k4/3Q4/4K3/8/8/8/8/8 b - -";
+	int playerWon = adap.getPlayerWon(boardStr);
+
+	ASSERT_EQ(playerWon, static_cast<int>(ceg::PieceColor::WHITE));
+}
+
+TEST(ReducedChessAdapter, test_get_player_won_none)
+{
+	const std::string boardStr = "3k4/8/4K3/3Q4/8/8/8/8 b - -";
+	int playerWon = adap.getPlayerWon(boardStr);
+
+	ASSERT_EQ(playerWon, static_cast<int>(ceg::PieceColor::NONE));
+}
+
+TEST(ReducedChessAdapter, test_is_game_over)
+{
+	const std::string boardStr = "3k4/2R1R3/8/8/8/8/8/5K2 b - -";
+
+	ASSERT_EQ(adap.isGameOver(boardStr), true);
+}
